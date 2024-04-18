9e8c1477aa2d2d2d5a6e0ce89b30269e01731bcf
Porawet Ku <gporawet.ku@gmail.com>
2024-04-18 18:07:08 +0700

    chore: fontend prime

---
+++ src/components/navbar.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import Image from "next/image";
+import { Avatar } from "primereact/avatar";
+import { Menubar } from "primereact/menubar";
+import { MenuItem } from "primereact/menuitem";
+
+const Navbar = () => {
+  const start = <Image width={50} height={40} alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" className="mr-2" />;
+
+  const end = (
+    <div className="flex align-items-center gap-2">
+      <Avatar size="normal" image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png" shape="circle" />
+    </div>
+  );
+
+  return (
+    <div className="card">
+      <Menubar start={start} end={end} />
+    </div>
+  );
+};
+
+export { Navbar };
