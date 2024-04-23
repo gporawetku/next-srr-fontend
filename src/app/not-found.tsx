1934343cd5d3d505be21a0f061fb7476772eb741
PurinPintakhiew <dev.purin@gmail.com>
2024-04-23 10:50:51 +0700

    feat: not-found function

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function NotFound() {
+  const route = useRouter();
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="text-center">
+        <div className="text-2xl mb-2">ไม่พบหน้านี้ – 404!</div>
+        <div className="flex gap-2">
+          <button className="border border-blue-400 rounded-md p-2" onClick={() => route.push("/")}>
+            กลับเข้าสู่หน้าหลัก
+          </button>
+          <button type="button" className="border border-yellow-400 rounded-md p-2" onClick={() => route.back()}>
+            กลับหน้าไปยังหน้าล่าสุด
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
