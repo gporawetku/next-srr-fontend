45d36e0330afe4bb3c52007015a4714306511bd6
PurinPintakhiew <dev.purin@gmail.com>
2024-04-26 11:45:00 +0700

    chore: edit layout

--- src/Layouts/footer.tsx
+++ src/Layouts/footer.tsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const Footer = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // --- resize
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkIfMobile();
+    window.addEventListener("resize", checkIfMobile);
+
+    return () => window.removeEventListener("resize", checkIfMobile);
+  }, []);
+
   return (
     <>
-      <div className="bg-white flex justify-between items-center w-full p-5" style={{ bottom: 0, position: "absolute", height: "100px", left: 0, right: 0 }}>
+      <div
+        className="bg-white flex justify-between items-center w-full p-5"
+        style={isMobile ? { bottom: 0, position: "fixed", height: "100px", left: 0, right: 0, fontSize: "14px" } : { bottom: 0, position: "absolute", height: "100px", left: 0, right: 0 }}
+      >
         <div>©Sirarom 2024 - All rights reserved</div>
         <div>compattana.com</div>
       </div>
--- src/app/(cms)/layout.tsx
+++ src/app/(cms)/layout.tsx
@@ -8,13 +8,13 @@ import { Footer } from "@/Layouts/footer";
 export default function CMSLayout({ children }: { children: React.ReactNode }) {
   return (
     <CustomProviders>
-      <main>
+      <main className="bg-[#F7F7F7]">
         <Navbar />
-        <div className="grid grid-cols-12">
-          <div className="hidden md:block md:col-span-2 h-screen sticky top-0 z-[999]">
+        <div className="grid grid-cols-12 h-screen">
+          <div className="hidden md:block md:col-span-2  sticky top-0 z-[999]">
             <LeftSidebar />
           </div>
-          <div className="col-span-12 md:col-span-10 bg-[#F7F7F7] relative">
+          <div className="col-span-12 md:col-span-10 relative bg-[#F7F7F7]">
             <div className="p-2 md:p-10 mb-[100px]">{children}</div>
             <Footer />
           </div>
